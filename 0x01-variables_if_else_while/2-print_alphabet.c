decd58ee8e66ad27ff974407f7fcc1489a56fabd
MissRukaria <rukarialynnette@gmail.com>
2022-12-09 04:17:38 -0800

    Print alphbet A to Z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - main function to return alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
