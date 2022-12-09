a079676f30b470d348f741e42d6e3f58cf9a6dbd
MissRukaria <rukarialynnette@gmail.com>
2022-12-09 07:26:39 -0800

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,15 +9,15 @@
 
 int main(void)
 {
-	int num ='0'; /*Initialize num to 0*/
-	char letter ='a'; /*Initialize letter to a*/
+	int num = '0'; /*Initialize num to 0*/
+	char letter = 'a'; /*Initialize letter to a*/
 
 	while (num <= '9') /*Print 0-9*/
 	{
 		putchar(num);
 		num++;
 	}
-	while (letter <= 'a') /*Print a-f*/
+	while (letter <= 'f') /*Print a-f*/
 	{
 		putchar(letter);
 		letter++;
