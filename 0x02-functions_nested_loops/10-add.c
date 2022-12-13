9a1903f95064a587288098b71145f2ee49daae3b
MissRukaria <rukarialynnette@gmail.com>
2022-12-13 07:48:25 -0800

    Adds two integers and returns the value

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Entry point for the addition
+ * @a: First value to be added
+ * @b: Second value to be added
+ * Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+	int (a + b);
+}
+
