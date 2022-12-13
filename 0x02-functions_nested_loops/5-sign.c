10c82f8a0df1c8a1319fc8d54d5094ee8ae556e3
MissRukaria <rukarialynnette@gmail.com>
2022-12-13 04:23:12 -0800

    Prints sign before a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _sign - return 0 if n is 0 return 1 if n is positive return -1 if n is negative
+ *
+ * @n: number to check
+ * Return: 0 1 0r -1
+ *
+ */
+
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else
+	{
+		test - -1;
+		_putchar('-');
+	}
+	return (test);
+} 
