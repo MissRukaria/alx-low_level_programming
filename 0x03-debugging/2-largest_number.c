71005343f5270278220df8279a10705d4024971b
MissRukaria <rukarialynnette@gmail.com>
2022-12-13 23:12:09 -0800

    Largest of three numbers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,23 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a >= b && a >= c)
+	int largest;
+
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= a && b >= c)
+	else if (b > a && b > c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 	return (largest);
 }
